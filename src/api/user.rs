fa56c8b7a94f544e8a0405a8daee27477983ae69
Guillaume Desktop <robertohelper@outlook.com>
2024-06-16 21:54:05 +0200

    post user

--- src/api/user.rs
+++ src/api/user.rs
@@ -1,12 +1,12 @@
 use std::sync::Arc;
-use axum::{extract::{Path, State}, response::IntoResponse, routing::{get, post, Router}, Json};
+use axum::{extract::{self, Path, State}, response::IntoResponse, routing::{get, post, Router}, Json};
 use mongodb::Client;
+use serde::Deserialize;
 use serde_json::{json, Value};
 use derive_more::{Display};
 
 #[path ="../model/mod.rs"]
 mod model;
-
 use model::user::User;
 
 pub fn routes_user(state: Arc<Client>) -> Router
@@ -16,6 +16,8 @@ pub fn routes_user(state: Arc<Client>) -> Router
     .route("/user", post(post_user))
     .with_state(state)
 }
+
+const DB_NAME: &str = "user";
 
 #[derive(Debug, Display)]
 pub enum UserError
@@ -31,11 +33,20 @@ async fn get_user(Path(uuid): Path<String>)
 
 }
 
-async fn post_user(State(client): State<Arc<Client>>) 
+#[derive(Deserialize)]
+pub struct CreateUserRequest
 {
-    let accounts = client.database("chat").collection::<User>("accounts");
+    user_name: String,
+    user_mail: String,
+}
 
-    let account = User::new(String::from("Name"), String::from("mail"));
+async fn post_user(
+    State(client): State<Arc<Client>>, 
+    extract::Json(payload): extract::Json<CreateUserRequest>) 
+{
+    let accounts = client.database(DB_NAME).collection::<User>("accounts");
+    
+    let account = User::new(payload.user_name, payload.user_mail);
 
     accounts.insert_one(account, None).await;
 }
