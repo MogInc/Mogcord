e8ce15936b03c12aeeceb5158dc998f61b24e105
Guillaume Desktop <robertohelper@outlook.com>
2024-06-18 00:15:52 +0200

    beefed up convert_to_db

--- src/db/mongoldb/model/mongol_user.rs
+++ src/db/mongoldb/model/mongol_user.rs
@@ -1,6 +1,6 @@
 use mongodb::bson::oid::ObjectId;
 use serde::{Serialize, Deserialize};
-use crate::model::user::User;
+use crate::{db::mongoldb::MongolDB, model::user::User};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct MongolUser
@@ -13,7 +13,38 @@ pub struct MongolUser
 
 impl MongolUser
 {
-    pub fn convert_to_db(user : &User) -> MongolUser
+    pub async fn convert_to_db(
+        user : &User, 
+        mongol_db_option : Option<&MongolDB>
+    ) -> MongolUser
+    {
+
+        if let Some(mongol_db) = mongol_db_option {
+            match mongol_db.get_user_db_object_by_id(&user.uuid).await
+            {
+                Ok(user_from_db) => 
+                {
+                    return MongolUser
+                    {
+                        _id: user_from_db._id.clone(),
+                        uuid: user_from_db.uuid.clone(),
+                        name: user_from_db.name.clone(),
+                        mail: user_from_db.mail.clone()
+                    }
+                },
+                Err(_) => return Self::new_mongol_user(&user),
+            }
+        }
+        return Self::new_mongol_user(&user)
+    }
+
+    pub fn convert_to_domain(self) -> User
+    {
+        User::convert(self.uuid, self.name, self.mail)
+    }
+
+    fn new_mongol_user(user : &User)
+        ->  MongolUser
     {
         MongolUser
         {
@@ -22,10 +53,5 @@ impl MongolUser
             name: user.name.clone(),
             mail: user.mail.clone()
         }
-    }
-
-    pub fn convert_to_domain(self) -> User
-    {
-        User::convert(self.uuid, self.name, self.mail)
     }
 }
