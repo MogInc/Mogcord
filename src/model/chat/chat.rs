4399050434fc738894d360e488e5e7018781dfd2
Guillaume Desktop <robertohelper@outlook.com>
2024-06-18 02:00:34 +0200

    implemented base bucket, added message flag

--- src/model/chat/chat.rs
+++ src/model/chat/chat.rs
@@ -1,3 +1,5 @@
+use chrono::{DateTime, Utc};
+
 use crate::model::user::User;
 
 pub enum ChatType
@@ -14,12 +16,20 @@ pub struct Chat
     pub r#type: ChatType,
     pub owners: Vec<User>,
     pub members: Option<Vec<User>>,
-    pub buckets: Option<Vec<Bucket>>
+    pub buckets: Option<Vec<Bucket>>,
 }
 
 pub struct Bucket
 {
+    pub chat: Chat,
+    pub date: DateTime<Utc>,
+    pub messages: Option<Vec<Message>>,
+}
 
+pub enum MessageFlag
+{
+    None,
+    Deleted,
 }
 
 pub struct Message
@@ -29,4 +39,7 @@ pub struct Message
     pub owner: User,
     pub chat: Chat,
     pub bucket: Bucket,
+    //we actually gonna delete stuff?
+    //(:sins:)
+    pub flag: MessageFlag,
 }
