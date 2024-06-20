c8411181061cd8a70427d442cc4858915b6fdde8
Guillaume Desktop <robertohelper@outlook.com>
2024-06-20 18:50:31 +0200

    added ivnalid uuid error type

--- src/db/mongoldb/model/mongol_error.rs
+++ src/db/mongoldb/model/mongol_error.rs
@@ -3,13 +3,17 @@ use std::fmt;
 #[derive(Debug)]
 pub enum MongolError
 {
-    FailedUserParsing
+    InvalidUUID,
+    FailedUserParsing,
+    FailedChatParsing,
 }
 
 impl fmt::Display for MongolError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
+            MongolError::InvalidUUID => write!(f, "Invalid UUID"),
             MongolError::FailedUserParsing => write!(f, "Couldn't parse user"),
+            MongolError::FailedChatParsing => write!(f, "Couldn't parse chat"),
         }
     }
 }
