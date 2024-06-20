76968e9530dca3c0d7c06082d9c2862f91570ffe
Guillaume Desktop <robertohelper@outlook.com>
2024-06-20 18:47:48 +0200

    added chat repo

---
+++ src/model/chat/chat_repository.rs
@@ -0,0 +1,10 @@
+use axum::async_trait;
+
+use super::{Chat, ChatError};
+
+#[async_trait]
+pub trait ChatRepository: Send + Sync 
+{
+    async fn create_chat(&self, user: Chat) -> Result<Chat, ChatError>;
+    async fn get_chat_by_id(&self, chat_id: &String) -> Result<Chat, ChatError>;
+}
