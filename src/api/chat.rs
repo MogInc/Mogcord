c652609a757e62bd1a9f7a650b9c39a476d35b62
Guillaume Desktop <robertohelper@outlook.com>
2024-06-20 20:25:58 +0200

    testing chat repo

--- src/api/chat.rs
+++ src/api/chat.rs
@@ -1,9 +1,9 @@
 use std::sync::Arc;
-use axum::{extract::{Path, State}, response::IntoResponse, routing::{get, post}, Json, Router};
+use axum::{extract::{self, Path, State}, response::IntoResponse, routing::{get, post}, Json, Router};
 
-use crate::{db::mongoldb::MongolDB, model::chat::ChatRepository};
+use crate::{db::mongoldb::MongolDB, model::{chat::{Chat, ChatRepository}, user::User}};
 
-pub fn routes_user(state: Arc<MongolDB>) -> Router
+pub fn routes_chat(state: Arc<MongolDB>) -> Router
 {
     Router::new()
     .route("/chat/:id", get(get_chat))
@@ -16,14 +16,32 @@ async fn get_chat(
     Path(uuid): Path<String>) 
     -> impl IntoResponse
 {
-    match db.get_chat_by_id(&uuid).await 
-    {
-        Ok(chat) => Ok(Json(chat)),
-        Err(e) => Err(e),
-    }
+
+
+    // match db.get_chat_by_id(&uuid).await 
+    // {
+    //     Ok(chat) => Ok(Json(chat)),
+    //     Err(e) => Err(e),
+    // }
 }
 
-async fn post_chat()
+struct CreateChatRequest
 {
 
+}
+
+async fn post_chat(
+    State(db): State<Arc<dyn ChatRepository>>,
+    extract::Json(payload): extract::Json<CreateChatRequest>)
+ -> impl IntoResponse
+{
+    let mut users: Vec<crate::model::user::User> = Vec::new();
+    users.push(User::new(String::from("Ted"), String::from("Ted@shit.com")));
+    let chat = Chat::new(Some(String::from("W in da chat")), crate::model::chat::ChatType::Group, users.clone(),Some(users.clone()), None);
+
+    match db.create_chat(chat).await 
+    {
+        Ok(user) => Ok(Json(user)),
+        Err(e) => Err(e),
+    }
 }
