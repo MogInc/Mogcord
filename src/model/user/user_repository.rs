d5ddf26866394396bfe5e92280167eb3c2d58296
Guillaume Desktop <robertohelper@outlook.com>
2024-06-25 02:55:52 +0200

    added get_users to repo trait

--- src/model/user/user_repository.rs
+++ src/model/user/user_repository.rs
@@ -1,6 +1,6 @@
 use axum::async_trait;
 
-use crate::model::error::ServerError;
+use crate::model::{error::ServerError, pagination::Pagination};
 
 use super::user::User;
 
@@ -11,5 +11,6 @@ pub trait UserRepository: Send + Sync
     async fn does_user_exist_by_mail(&self, user_mail: &String) -> Result<bool, ServerError>;
     async fn create_user(&self, user: User) -> Result<User, ServerError>;
     async fn get_user_by_id(&self, user_id: &String) -> Result<User, ServerError>;
-    async fn get_users_by_id(&self, user_ids: Vec<String>) -> Result<Vec<User>, ServerError>;
+    async fn get_users(&self, pagination: Pagination) -> Result<Vec<User>, ServerError>;
+    async fn get_users_by_ids(&self, user_ids: Vec<String>) -> Result<Vec<User>, ServerError>;
 }
