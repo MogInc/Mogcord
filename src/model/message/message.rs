8c5019e6716e2247e992598b5d42527bbc874750
Guillaume Desktop <robertohelper@outlook.com>
2024-06-26 02:23:18 +0200

    added ctor

--- src/model/message/message.rs
+++ src/model/message/message.rs
@@ -1,5 +1,6 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 use crate::model::{chat::{Bucket, Chat}, user::User};
 
@@ -17,4 +18,24 @@ pub struct Message
     //we actually gonna delete stuff?
     //(:sins:)
     pub flag: MessageFlag,
+}
+
+impl Message {
+    pub fn new(
+        value: String, 
+        owner: User,
+        chat: Chat
+    ) -> Self
+    {
+        Self
+        {
+            uuid: Uuid::new_v4().to_string(),
+            value: value,
+            timestamp: Utc::now(),
+            owner: owner,
+            chat: chat,
+            bucket: None,
+            flag: MessageFlag::None
+        }
+    }
 }
