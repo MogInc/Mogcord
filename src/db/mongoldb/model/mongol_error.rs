85aa26197e2c5c3523d8512249283daf6b5543a5
Guillaume Desktop <robertohelper@outlook.com>
2024-07-02 04:08:39 +0200

    can now see values of error messages for log

--- src/db/mongoldb/model/mongol_error.rs
+++ src/db/mongoldb/model/mongol_error.rs
@@ -1,10 +1,20 @@
-use strum_macros::Display;
+use std::fmt;
+use serde::Serialize;
 
-#[derive(Debug, Display)]
+#[derive(Debug, Serialize)]
+#[serde(tag = "type", content = "data")]
 pub enum MongolError
 {
-    InvalidID,
+    InvalidID { id: String },
     FailedUserParsing,
     FailedChatParsing,
     FailedDateParsing,
+}
+
+impl fmt::Display for MongolError 
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result 
+	{
+        write!(f, "{self:?}")
+    }
 }
