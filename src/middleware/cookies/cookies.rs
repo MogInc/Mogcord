451d4f6f61a47c91a599e05debe7e8c58cf2ef06
Guillaume Desktop <robertohelper@outlook.com>
2024-07-04 01:07:58 +0200

    renamed to create cookie since i dont return a builder anymore

--- src/middleware/cookies/cookies.rs
+++ src/middleware/cookies/cookies.rs
@@ -1,6 +1,6 @@
 use tower_cookies::{cookie::{time::{Duration, OffsetDateTime}, SameSite}, Cookie};
 
-pub fn create_base_cookie<'a>(name: String, value: String, ttl_in_mins: i64) -> Cookie<'a>
+pub fn create_cookie<'a>(name: String, value: String, ttl_in_mins: i64) -> Cookie<'a>
 {
     let cookie = Cookie::build((name, value))
         .path("/")
