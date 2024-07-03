0b8eb49eed54b55250e13b25077affbcc86046ba
Guillaume Desktop <robertohelper@outlook.com>
2024-07-04 01:54:17 +0200

    login request

--- src/api/auth_handler.rs
+++ src/api/auth_handler.rs
@@ -1,33 +1,54 @@
 use std::sync::Arc;
 
-use axum::{response::IntoResponse, routing::get, Json, Router};
-use tower_cookies::{Cookie, Cookies};
+use axum::{extract::State, response::IntoResponse, routing::{get, post}, Json, Router};
+use serde::Deserialize;
+use tower_cookies::Cookies;
 
-use crate::{middleware::{cookies::{self, AuthCookieNames}, jwt}, model::{misc::{AppState, ServerError}, user::User}};
+use crate::{middleware::{cookies::{self, AuthCookieNames}, jwt, Ctx}, model::misc::AppState};
 
 pub fn routes_auth(state: Arc<AppState>) -> Router
 {
     Router::new()
-    .route("/auth", get(test))
+    .route("/auth/login", post(login))
+    .route("/auth/refresh", get(refresh_token))
     .with_state(state)
 }
 
-async fn test(cookies: Cookies) -> impl IntoResponse
+#[derive(Deserialize)]
+struct LoginRequest
 {
+    mail: String,
+}
+
+async fn login(
+    State(state): State<Arc<AppState>>,
+    cookies: Cookies, 
+    Json(payload): Json<LoginRequest>,
+) -> impl IntoResponse
+{
+    let repo_user = &state.repo_user;
+
+    let user = repo_user
+        .get_user_by_mail(&payload.mail)
+        .await?;
+
     let auth_cookie_name: &str = AuthCookieNames::AUTH_TOKEN.into();
 
-    let random_user = User::new(String::from("Snibert"), String::from("Sni@bert.com"));
-
-    match jwt::create_token(&random_user)
+    match jwt::create_token(&user)
     {
         Ok(token) => 
         {
-            let cookiebuilder = cookies::create_base_cookie(auth_cookie_name.to_string(), token, cookies::JWT_COOKIE_TTL_MINS);
+            let cookie_auth = cookies::create_cookie(auth_cookie_name.to_string(), token, cookies::JWT_COOKIE_TTL_MINS);
 
-            cookies.add(cookiebuilder);
+            cookies.add(cookie_auth);
 
             return Ok(());
         },
         Err(err) => Err(err),
     }
+}
+
+async fn refresh_token(cookies: Cookies, ctx: Ctx)
+{
+    
 }
