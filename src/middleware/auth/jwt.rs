1f73c5bb0c7fa4cb77d2c1449e9841a143acf785
Guillaume Desktop <robertohelper@outlook.com>
2024-07-03 18:05:23 +0200

    implemented decode

--- src/middleware/auth/jwt.rs
+++ src/middleware/auth/jwt.rs
@@ -1,13 +1,23 @@
+use std::env;
+
 use chrono::{Duration, Utc};
-use jsonwebtoken::{encode, EncodingKey, Header};
+use jsonwebtoken::{decode, encode, errors::ErrorKind, DecodingKey, EncodingKey, Header, Validation};
 use serde::{Deserialize, Serialize};
 use crate::model::misc::ServerError;
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Claims
+pub struct Claims
 {
     sub: String,
     exp: usize,
+}
+
+impl Claims
+{
+    pub fn sub(self) -> String
+    {
+        return self.sub;
+    }
 }
 
 pub fn create_token() -> Result<String, ServerError>
@@ -18,17 +28,33 @@ pub fn create_token() -> Result<String, ServerError>
         exp: (Utc::now() + Duration::minutes(10)).timestamp() as usize,
     };
     
+    let jwt_key = env::var("JWT_KEY")
+        .map_err(|_| ServerError::JWTKeyNotSet)?;
+
     let token = encode(
         &Header::default(), 
         &claims, 
-        &EncodingKey::from_secret("JWT_KEY".as_ref())
+        &EncodingKey::from_secret(jwt_key.as_ref())
     ).map_err(|_| ServerError::FailedCreatingToken)?;
 
 
     Ok(token)
 }
 
-pub fn verify_token() -> Result<bool, ServerError>
+pub fn extract_token(token: &str) -> Result<Claims, ServerError>
 {
+    let jwt_key = env::var("JWT_KEY")
+        .map_err(|_| ServerError::JWTKeyNotSet)?;
 
+    match decode::<Claims>(token,&DecodingKey::from_secret(jwt_key.as_ref()), &Validation::default())
+    {
+        Ok(token_data) => Ok(token_data.claims),
+        Err(err) => {
+            match *err.kind()
+            {
+                ErrorKind::ExpiredSignature => Err(ServerError::JWTTokenExpired),
+                _ => Err(ServerError::JWTTokenInvalid)
+            }
+        },
+    }
 }
