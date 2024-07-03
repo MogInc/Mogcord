38661832758fb568d7098957e95c2a3683119d43
Guillaume Desktop <robertohelper@outlook.com>
2024-07-03 23:10:01 +0200

    added cookie folder

---
+++ src/middleware/cookies/cookies.rs
@@ -0,0 +1,14 @@
+use tower_cookies::{cookie::{time::{Duration, OffsetDateTime}, SameSite}, Cookie};
+
+pub fn create_base_cookie<'a>(name: String, value: String, ttl_in_mins: i64) -> Cookie<'a>
+{
+    let cookie = Cookie::build((name, value))
+        .path("/")
+        .http_only(true)
+        .secure(true)
+        .same_site(SameSite::Lax)
+        .expires(OffsetDateTime::now_utc() + Duration::minutes(ttl_in_mins))
+        .build();
+
+    return cookie;
+}
---
+++ src/middleware/cookies/mod.rs
@@ -0,0 +1,5 @@
+mod cookies;
+mod auth_cookie_names;
+
+pub use cookies::*;
+pub use auth_cookie_names::*;
