6fec1c2c3dddddf3fbd6f0f89fbee2a6c8ee279c
Guillaume Desktop <robertohelper@outlook.com>
2024-07-03 23:11:50 +0200

    added print for debug

--- src/api/auth_handler.rs
+++ src/api/auth_handler.rs
@@ -1,8 +1,9 @@
 use std::sync::Arc;
 
 use axum::{response::IntoResponse, routing::get, Json, Router};
+use tower_cookies::{Cookie, Cookies};
 
-use crate::model::misc::AppState;
+use crate::{middleware::{cookies::{self, AuthCookieNames}, jwt}, model::{misc::{AppState, ServerError}, user::User}};
 
 pub fn routes_auth(state: Arc<AppState>) -> Router
 {
@@ -11,7 +12,22 @@ pub fn routes_auth(state: Arc<AppState>) -> Router
     .with_state(state)
 }
 
-async fn test() -> impl IntoResponse
+async fn test(cookies: Cookies) -> impl IntoResponse
 {
-    Json("test")
+    let auth_cookie_name: &str = AuthCookieNames::AUTH_TOKEN.into();
+
+    let random_user = User::new(String::from("Snibert"), String::from("Sni@bert.com"));
+
+    match jwt::create_token(&random_user)
+    {
+        Ok(token) => 
+        {
+            let cookiebuilder = cookies::create_base_cookie(auth_cookie_name.to_string(), token, jwt::JWT_TTL_MINS);
+
+            cookies.add(cookiebuilder);
+
+            return Ok(());
+        },
+        Err(err) => Err(err),
+    }
 }
