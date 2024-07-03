28aede63b32c27b7079f7fcc5ac19baf210bf883
Guillaume Desktop <robertohelper@outlook.com>
2024-07-04 01:54:59 +0200

    added refresh token file

--- src/middleware/auth/mod.rs
+++ src/middleware/auth/mod.rs
@@ -3,6 +3,8 @@ pub mod jwt;
 
 mod mw_auth;
 mod ctx;
+mod refresh_token_creator;
 
 pub use mw_auth::*;
 pub use ctx::*;
+pub use refresh_token_creator::*;
---
+++ src/middleware/auth/refresh_token_creator.rs
@@ -0,0 +1,4 @@
+pub fn create_refresh_token()
+{
+
+}
