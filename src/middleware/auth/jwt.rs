9630a8eb127666b24a3496b16a36ebb6f5ef9c15
Guillaume Desktop <robertohelper@outlook.com>
2024-07-03 23:10:34 +0200

    added TTL const for jwt

--- src/middleware/auth/jwt.rs
+++ src/middleware/auth/jwt.rs
@@ -3,7 +3,9 @@ use std::env;
 use chrono::{Duration, Utc};
 use jsonwebtoken::{decode, encode, errors::ErrorKind, DecodingKey, EncodingKey, Header, Validation};
 use serde::{Deserialize, Serialize};
-use crate::model::misc::ServerError;
+use crate::model::{misc::ServerError, user::User};
+
+pub const JWT_TTL_MINS: i64 = 10;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Claims
@@ -18,14 +20,19 @@ impl Claims
     {
         return self.sub;
     }
+
+    pub fn exp(self) -> usize
+    {
+        return self.exp;
+    }
 }
 
-pub fn create_token() -> Result<String, ServerError>
+pub fn create_token(user: &User) -> Result<String, ServerError>
 {
     let claims = Claims
     {
-        sub: String::from("Appel"),
-        exp: (Utc::now() + Duration::minutes(10)).timestamp() as usize,
+        sub: user.id.clone(),
+        exp: (Utc::now() + Duration::minutes(JWT_TTL_MINS)).timestamp() as usize,
     };
     
     let jwt_key = env::var("JWT_KEY")
