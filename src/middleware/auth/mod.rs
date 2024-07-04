ed065aa18190482e56303f7c81ad1fba29a51167
Guillaume Desktop <robertohelper@outlook.com>
2024-07-04 02:30:14 +0200

    implemented refresh token creator

--- src/middleware/auth/mod.rs
+++ src/middleware/auth/mod.rs
@@ -1,10 +1,9 @@
 pub mod jwt;
+pub mod refresh_token_creator;
 
 
 mod mw_auth;
 mod ctx;
-mod refresh_token_creator;
 
 pub use mw_auth::*;
 pub use ctx::*;
-pub use refresh_token_creator::*;
--- src/middleware/auth/refresh_token_creator.rs
+++ src/middleware/auth/refresh_token_creator.rs
@@ -1,4 +1,16 @@
-pub fn create_refresh_token()
-{
+use argon2::password_hash::rand_core::{OsRng, RngCore};
+use base64::{alphabet, engine::{general_purpose, GeneralPurpose}, Engine};
 
+pub fn create_refresh_token() -> String
+{
+    let mut random_number = [0u8; 64];
+        
+    let mut rng = OsRng;
+    rng.fill_bytes(&mut random_number);
+    
+    const CUSTOM_ENGINE: GeneralPurpose = GeneralPurpose::new(&alphabet::URL_SAFE, general_purpose::NO_PAD);
+
+    let refresh_token = CUSTOM_ENGINE.encode(&random_number);
+    
+    return refresh_token;
 }
