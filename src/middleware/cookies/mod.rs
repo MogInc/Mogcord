3594f5e2a972d8c360af8445f1446b2476764a9c
Guillaume Desktop <robertohelper@outlook.com>
2024-07-04 02:28:10 +0200

    added some more ttls

--- src/middleware/cookies/mod.rs
+++ src/middleware/cookies/mod.rs
@@ -4,4 +4,6 @@ mod auth_cookie_names;
 pub use cookies::*;
 pub use auth_cookie_names::*;
 
-pub const JWT_COOKIE_TTL_MINS: i64 = 60 * 24 * 31;
+pub const COOKIE_ACCES_TOKEN_TTL_MIN: i64 = 60 * 24 * 31;
+pub const COOKIE_REFRESH_TOKEN_TTL_MIN: i64 = 60 * 24 * 365;
+pub const COOKIE_DEVICE_ID_TTL_MIN: i64 = 60 * 24 * 365;
