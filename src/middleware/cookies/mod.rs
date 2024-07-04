6a4981c06dd4fd2b9eb40d16dc55558866f634a5
Guillaume Desktop <robertohelper@outlook.com>
2024-07-04 02:31:21 +0200

    changed device id cookie ttl

--- src/middleware/cookies/mod.rs
+++ src/middleware/cookies/mod.rs
@@ -6,4 +6,4 @@ pub use auth_cookie_names::*;
 
 pub const COOKIE_ACCES_TOKEN_TTL_MIN: i64 = 60 * 24 * 31;
 pub const COOKIE_REFRESH_TOKEN_TTL_MIN: i64 = 60 * 24 * 365;
-pub const COOKIE_DEVICE_ID_TTL_MIN: i64 = 60 * 24 * 365;
+pub const COOKIE_DEVICE_ID_TTL_MIN: i64 = 60 * 24 * 365 * 5;
