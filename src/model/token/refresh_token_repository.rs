224c6b055b9e4fa2e13115c32c253cec37232ec8
Guillaume Desktop <robertohelper@outlook.com>
2024-07-06 01:37:34 +0200

    added method

--- src/model/token/refresh_token_repository.rs
+++ src/model/token/refresh_token_repository.rs
@@ -1,8 +1,12 @@
 use axum::async_trait;
 
+use crate::model::misc::ServerError;
+
+use super::RefreshToken;
+
 
 #[async_trait]
 pub trait RefreshTokenRepository: Send + Sync 
 {
-
+    async fn get_token_by_device_id(&self, device_id: &str) -> Result<RefreshToken, ServerError>;
 }
