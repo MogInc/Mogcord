d26c3ab40ed2c156bc5ebdbfbcb375da63bd7906
Guillaume Desktop <robertohelper@outlook.com>
2024-07-06 01:56:23 +0200

    implemented get_token_by_device_id

--- src/db/mongoldb/repositories/refresh_token_repository.rs
+++ src/db/mongoldb/repositories/refresh_token_repository.rs
@@ -1,12 +1,26 @@
 use axum::async_trait;
+use bson::{doc, Uuid};
 
-use crate::{db::mongoldb::MongolDB, model::{misc::ServerError, token::{RefreshToken, RefreshTokenRepository}}};
+use crate::{db::mongoldb::{mongol_helper, MongolDB}, model::{misc::ServerError, token::{RefreshToken, RefreshTokenRepository}}};
 
 #[async_trait]
 impl RefreshTokenRepository for MongolDB
 {
     async fn get_token_by_device_id(&self, device_id: &str) -> Result<RefreshToken, ServerError>
     {
-        Err(ServerError::ChatAlreadyExists)
+        let device_id_local: Uuid = mongol_helper::convert_domain_id_to_mongol(&device_id)
+            .map_err(|_| ServerError::RefreshTokenNotFound)?;
+
+        let token_option = self
+            .refresh_tokens()
+            .find_one(doc! { "device_id" : device_id_local })
+            .await
+            .map_err(|err| ServerError::FailedRead(err.to_string()))?;
+
+        match token_option
+        {
+            Some(token) => Ok(RefreshToken::from(&token)),
+            None => Err(ServerError::RefreshTokenNotFound)
+        }
     }
 }
