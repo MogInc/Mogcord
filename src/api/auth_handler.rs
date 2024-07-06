a137477c82a6317d911e9212f976ee1ba7f1b82c
Guillaume Desktop <robertohelper@outlook.com>
2024-07-06 14:24:56 +0200

    made login handler more concise

--- src/api/auth_handler.rs
+++ src/api/auth_handler.rs
@@ -38,27 +38,27 @@ async fn login(
     let _ = Hashing::verify_hash(&payload.password, &user.hashed_password).await?;
 
     //either 
-    //if user has a device id, token up if exists and use that.
-    //say frog it and keep genning new ones
+    //1: if user has a device id, token up if exists and use that.
+    //2: say frog it and keep genning new ones
 
     let refresh_token_option = CookieManager::get_cookie(&cookies,AuthCookieNames::AUTH_REFRESH.into());
     let device_id_option = CookieManager::get_cookie(&cookies,AuthCookieNames::DEVICE_ID.into());
 
     let mut refresh_token: RefreshToken = RefreshToken::create_token();
-    let mut create_new_token = false;
+    let mut create_new_token = true;
 
 
-    if let (Some(device_id), Some(_refresh_token)) = (device_id_option, refresh_token_option)
+    if let (Some(device_id_cookie), Some(_refresh_token_cookie)) = (device_id_option, refresh_token_option)
     {
-        match repo_refresh.get_token_by_device_id(&device_id).await
+        match repo_refresh.get_token_by_device_id(&device_id_cookie).await
         {
-            Ok(token) => refresh_token = token,
-            Err(_) => create_new_token = true,
+            Ok(token) => 
+            {
+                refresh_token = token;
+                create_new_token = false;
+            },
+            _ => (),
         }
-    }
-    else 
-    {
-        create_new_token = true;
     }
 
     if create_new_token
@@ -72,6 +72,7 @@ async fn login(
             refresh_token.value, 
             cookies::COOKIE_REFRESH_TOKEN_TTL_MIN
         );
+
         let cookie_device_id = CookieManager::create_cookie(
             AuthCookieNames::DEVICE_ID.into(), 
             refresh_token.device_id, 
