f0755ab6542dfe526abad712d620fa8b23e8ad65
Guillaume Desktop <robertohelper@outlook.com>
2024-07-07 23:11:39 +0200

    added comment

--- src/db/mongoldb/repositories/message_repository.rs
+++ src/db/mongoldb/repositories/message_repository.rs
@@ -242,6 +242,10 @@ impl MessageRepository for MongolDB
             .await
             .map_err(|err| ServerError::FailedRead(err.to_string()))?;
 
+        //what would be faster
+        //1: reallocating vecs when capacity is reached
+        //2: having a count or length and setting a capacity 
+
         let mut messages: Vec<Message> = Vec::new();
 
         while let Some(result) = cursor.next().await
