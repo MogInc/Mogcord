ab10446a449196084d970dc522aa5479bbf3f4e1
Guillaume Desktop <robertohelper@outlook.com>
2024-07-09 01:17:56 +0200

    mod contains token ttl

--- src/middleware/auth/mod.rs
+++ src/middleware/auth/mod.rs
@@ -7,6 +7,6 @@ mod ctx;
 pub use mw_auth::*;
 pub use ctx::*;
 
-pub const ACCES_TOKEN_TTL_MIN: i64 = 60 * 24 * 31;
+pub const ACCES_TOKEN_TTL_MIN: i64 = 10;
 pub const REFRESH_TOKEN_TTL_MIN: i64 = 60 * 24 * 365;
 pub const DEVICE_ID_TTL_MIN: i64 = 60 * 24 * 365 * 5;
