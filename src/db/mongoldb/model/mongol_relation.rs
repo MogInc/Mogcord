767b1b51906e6c96c09f322ce853a1eb99d666dc
Guillaume Desktop <robertohelper@outlook.com>
2024-07-15 01:42:58 +0200

    feature: impl Mongorelation

--- src/db/mongoldb/model/mongol_relation.rs
+++ src/db/mongoldb/model/mongol_relation.rs
@@ -3,5 +3,9 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize)]
 pub struct MongolRelation
 {
-
+    user_id: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    friend_ids: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    blocked_ids: Option<String>,
 }
