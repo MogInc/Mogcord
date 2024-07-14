74f9f25c543ba640b2286b81c0deb834a2565175
Guillaume Desktop <robertohelper@outlook.com>
2024-07-15 00:55:50 +0200

    feature: added base relation repository methods

--- src/model/relation/relation_repository.rs
+++ src/model/relation/relation_repository.rs
@@ -1,7 +1,10 @@
 use axum::async_trait;
 
+use crate::model::misc::ServerError;
+
 #[async_trait]
 pub trait RelationRepository: Send + Sync
 {
-
+    async fn does_friendship_exist(&self, current_user_id: &str, other_user_id: &str) -> Result<bool, ServerError>;
+    async fn add_friend(&self, current_user_id: &str, other_user_id: &str) -> Result<(), ServerError>;
 }
