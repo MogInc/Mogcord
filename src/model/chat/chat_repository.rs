961414a0a1cee35a200d9a70ea3ac60ff1ff45ae
Guillaume Desktop <robertohelper@outlook.com>
2024-07-17 14:37:38 +0200

    feature: added get_chat_by_id in chat repo trait

--- src/model/chat/chat_repository.rs
+++ src/model/chat/chat_repository.rs
@@ -8,6 +8,7 @@ use super::Chat;
 pub trait ChatRepository: Send + Sync 
 {
     async fn create_chat(&self, chat: Chat) -> Result<Chat, ServerError>;
+    async fn get_chat_by_id(&self, chat_id: &str) -> Result<Chat, ServerError>;
     async fn get_chat_by_chat_info_id(&self, chat_info_id: &str) -> Result<Chat, ServerError>;
     async fn does_chat_exist(&self, chat: &Chat) -> Result<bool, ServerError>;
 }
