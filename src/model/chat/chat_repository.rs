903df99d4aeff75f301aff684338f59974124a32
Guillaume Desktop <robertohelper@outlook.com>
2024-07-17 13:54:55 +0200

    Revert "feature: added get_chat_info_by_id in repo trait"
    This reverts commit a43e5cc764f8c0dafb724fe5acfc4b1768cd485a.

--- src/model/chat/chat_repository.rs
+++ src/model/chat/chat_repository.rs
@@ -2,13 +2,12 @@ use axum::async_trait;
 
 use crate::model::misc::ServerError;
 
-use super::{Chat, ChatInfo};
+use super::Chat;
 
 #[async_trait]
 pub trait ChatRepository: Send + Sync 
 {
     async fn create_chat(&self, chat: Chat) -> Result<Chat, ServerError>;
     async fn get_chat_by_id(&self, chat_id: &str) -> Result<Chat, ServerError>;
-    async fn get_chat_info_by_id(&self, chat_info_id: &str) -> Result<ChatInfo, ServerError>;
     async fn does_chat_exist(&self, chat: &Chat) -> Result<bool, ServerError>;
 }
