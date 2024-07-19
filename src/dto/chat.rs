e67b215960272c5b0f8f35ee2eefb09b9a6a3e6e
Guillaume Desktop <robertohelper@outlook.com>
2024-07-19 18:48:08 +0200

    feature: added ChatGetResponse

--- src/dto/chat.rs
+++ src/dto/chat.rs
@@ -74,3 +74,74 @@ impl ObjectToDTO<Chat> for ChatCreateResponse
         }
     }
 }
+
+#[derive(Serialize)]
+pub struct ChatGetResponse
+{
+    id: String,
+    r#type: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    owner: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    owners: Option<Vec<String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    users: Option<Vec<String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    chat_info: Option<ChatInfoCreateResponse>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    chat_infos: Option<Vec<ChatInfoCreateResponse>>,
+}
+
+impl ObjectToDTO<Chat> for ChatGetResponse
+{
+    fn obj_to_dto(model_input: Chat) -> Self 
+    {
+        match model_input
+        {
+            Chat::Private { id, owners, chat_info } => 
+            {
+                Self
+                {
+                    id, 
+                    r#type: String::from( "Private"),
+                    name: None,
+                    owner: None,
+                    owners: Some(owners.into_iter().map(|user| user.id).collect()),
+                    users: None,
+                    chat_info: Some(ChatInfoCreateResponse::obj_to_dto(chat_info)),
+                    chat_infos: None,
+                }
+            },
+            Chat::Group { id, name, owner, users, chat_info } => 
+            {
+                Self
+                {
+                    id,
+                    r#type: String::from( "Group"),
+                    name: Some(name),
+                    owner: Some(owner.id),
+                    owners: None,
+                    users: Some(users.into_iter().map(|user| user.id).collect()),
+                    chat_info: Some(ChatInfoCreateResponse::obj_to_dto(chat_info)),
+                    chat_infos: None,
+                }
+            },
+            Chat::Server { id, name, owner, users, chat_infos } => 
+            {
+                Self
+                {
+                    id,
+                    r#type: String::from( "Server"),
+                    name: Some(name),
+                    owner: Some(owner.id),
+                    owners: None,
+                    users: Some(users.into_iter().map(|user| user.id).collect()),
+                    chat_info: None,
+                    chat_infos: Some(vec_to_dto(chat_infos))
+                }
+            },
+        }
+    }
+}
