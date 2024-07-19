880f06ad3075e283f1a06d86c9ea663e18dcd078
Guillaume Desktop <robertohelper@outlook.com>
2024-07-19 18:46:07 +0200

    fix: implemented  MessageGetResponse in message handler

--- src/handler/message.rs
+++ src/handler/message.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 use axum::{extract::{self, Path, Query, State}, middleware, response::IntoResponse, routing::{get, patch, post}, Json, Router};
 use serde::Deserialize;
 
-use crate::{dto::{vec_to_dto, MessageCreateResponse, ObjectToDTO}, middleware::auth::{self, Ctx}, model::{message::Message, {AppState, Pagination, error}}};
+use crate::{dto::{vec_to_dto, MessageCreateResponse, MessageGetResponse, ObjectToDTO}, middleware::auth::{self, Ctx}, model::{error, message::Message, AppState, Pagination}};
 
 pub fn routes(state: Arc<AppState>) -> Router
 {
@@ -39,7 +39,7 @@ async fn get_messages_for_authenticated(
 
     match repo_message.get_valid_messages(&chat_info_id, pagination).await
     {
-        Ok(messages) => Ok(Json(vec_to_dto::<Message, MessageCreateResponse>(messages))),
+        Ok(messages) => Ok(Json(vec_to_dto::<Message, MessageGetResponse>(messages))),
         Err(e) => Err(e),
     }
 }
