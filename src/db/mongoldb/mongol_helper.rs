dd5d0f1b4dc598a9b7ff08e3226c0a4ef13f93e3
Guillaume Desktop <robertohelper@outlook.com>
2024-07-19 03:33:01 +0200

    refactor: mongol_helper clippy

--- src/db/mongoldb/mongol_helper.rs
+++ src/db/mongoldb/mongol_helper.rs
@@ -21,6 +21,7 @@ impl MongolHelper for DateTime<Utc>
         MongolHelper::convert_to_bson_date(&date)
     }
 
+    #[allow(clippy::cast_possible_truncation)]
     fn convert_to_bson_datetime(&self) -> Result<bson::DateTime, bson::datetime::Error>
     {
         bson::DateTime::builder()
@@ -36,6 +37,7 @@ impl MongolHelper for DateTime<Utc>
 
 impl MongolHelper for NaiveDate
 {
+    #[allow(clippy::cast_possible_truncation)]
     fn convert_to_bson_date(&self) -> Result<bson::DateTime, bson::datetime::Error>
     {
         bson::DateTime::builder()
@@ -44,7 +46,8 @@ impl MongolHelper for NaiveDate
             .day(self.day() as u8)
             .build()
     }
-
+    
+    #[allow(clippy::cast_possible_truncation)]
     fn convert_to_bson_datetime(&self) -> Result<bson::DateTime, bson::datetime::Error>
     {
         bson::DateTime::builder()
