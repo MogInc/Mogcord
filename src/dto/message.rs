e51bd299b202f97ac1941bd37095203e833063b7
Guillaume Desktop <robertohelper@outlook.com>
2024-07-19 18:45:10 +0200

    feature: added MessageGetResponse

--- src/dto/message.rs
+++ src/dto/message.rs
@@ -34,3 +34,35 @@ impl ObjectToDTO<Message> for MessageCreateResponse
         }
     }
 }
+
+
+#[derive(Serialize)]
+pub struct MessageGetResponse
+{
+    id: String,
+    value: String,
+    timestamp: String,
+    owner_id: String,
+    chat_id: String,
+    bucket_id: String,
+    //we actually gonna delete stuff?
+    //(:sins:)
+    flag: String,
+}
+
+impl ObjectToDTO<Message> for MessageGetResponse
+{
+    fn obj_to_dto(message: Message) -> Self
+    {
+        Self
+        {
+            id: message.id,
+            value: message.value,
+            timestamp: message.timestamp.to_rfc3339(),
+            owner_id: message.owner.id,
+            chat_id: message.chat.id,
+            bucket_id: message.bucket_id.map_or(String::new(), |bucket| bucket),
+            flag: message.flag.to_string(),
+        }
+    }
+}
