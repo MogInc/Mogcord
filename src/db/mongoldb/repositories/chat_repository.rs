781c62574aa10355019b220b66731ceb6e89c350
Guillaume Desktop <robertohelper@outlook.com>
2024-07-19 14:37:46 +0200

    refactor: changed internal_x_chat_pipeline(filter: Doc) -> Vec<> signature to internal_x_chat_pipeline() -> []

--- src/db/mongoldb/repositories/chat_repository.rs
+++ src/db/mongoldb/repositories/chat_repository.rs
@@ -39,50 +39,65 @@ impl ChatRepository for MongolDB
             .map_err(|err| ServerError::FailedRead(err.to_string()))?
             .ok_or(ServerError::ChatNotFound)?;
 
-        let pipelines = match &mongol_chat_wrapper.chat
+        let pipeline = match &mongol_chat_wrapper.chat
         {
             MongolChat::Private { .. } => 
             {
-                let filter = doc! 
-                {
-                    "$match":
+                let mut pipeline = vec!
+                [
+                    doc! 
                     {
-                        "_id": chat_id_local
-                    }
-                };
-                
-                internal_private_chat_pipeline(filter)
+                        "$match":
+                        {
+                            "_id": chat_id_local
+                        }
+                    },
+                ];
+
+                pipeline.extend(internal_private_chat_pipeline());
+
+                pipeline
             },
             MongolChat::Group { .. } => 
             {
-                let filter = doc! 
-                {
-                    "$match":
+                let mut pipeline = vec!
+                [
+                    doc! 
                     {
-                        "_id": chat_id_local
-                    }
-                };
-                
-                internal_group_chat_pipeline(filter)
+                        "$match":
+                        {
+                            "_id": chat_id_local
+                        }
+                    },
+                ];
+
+                pipeline.extend(internal_group_chat_pipeline());
+
+                pipeline
             },
             MongolChat::Server { .. } => 
             {
-                let filter = doc! 
-                {
-                    "$match":
+                let mut pipeline = vec!
+                [
+                    doc! 
                     {
-                        "_id": chat_id_local
-                    }
-                };
+                        "$match":
+                        {
+                            "_id": chat_id_local
+                        }
+                    },
+                ];
 
-                internal_server_chat_pipeline(filter)
+                pipeline.extend(internal_server_chat_pipeline());
+
+                pipeline
             },
         };
             
 
         let mut cursor = self
             .chats()
-            .aggregate(pipelines)
+            .aggregate(pipeline)
             .await
             .map_err(|err| ServerError::FailedRead(err.to_string()))?;
     
@@ -132,50 +147,66 @@ impl ChatRepository for MongolDB
             .ok_or(ServerError::ChatNotFound)?;
 
 
-        let pipelines = match &mongol_chat_wrapper.chat
+        let pipeline = match &mongol_chat_wrapper.chat
         {
             MongolChat::Private { .. } => 
             {
-                let filter = doc! 
-                {
-                    "$match":
+                let mut pipeline = vec!
+                [
+                    doc! 
                     {
-                        "_id": chat_info_id_local
-                    }
-                };
+                        "$match":
+                        {
+                            "_id": chat_info_id_local
+                        }
+                    },
+                ];
 
-                internal_private_chat_pipeline(filter)
+                pipeline.extend(internal_private_chat_pipeline());
+
+                pipeline
             },
             MongolChat::Group { .. } => 
             {
-                let filter = doc! 
-                {
-                    "$match":
+                let mut pipeline = vec!
+                [
+                    doc! 
                     {
-                        "_id": chat_info_id_local
-                    }
-                };
-                
-                internal_group_chat_pipeline(filter)
+                        "$match":
+                        {
+                            "_id": chat_info_id_local
+                        }
+                    },
+                ];
+
+                pipeline.extend(internal_group_chat_pipeline());
+
+                pipeline
             },
             MongolChat::Server { .. } => 
             {
-                let filter = doc! 
-                {
-                    "$match":
-                    {
-                        "chat.Server.chat_infos._id": chat_info_id_local
-                    }
-                };
 
-                internal_server_chat_pipeline(filter)
+                let mut pipeline = vec!
+                [
+                    doc! 
+                    {
+                        "$match":
+                        {
+                            "chat.Server.chat_infos._id": chat_info_id_local
+                        }
+                    },
+                ];
+
+                pipeline.extend(internal_server_chat_pipeline());
+
+                pipeline
             },
         };
             
 
         let mut cursor = self
             .chats()
-            .aggregate(pipelines)
+            .aggregate(pipeline)
             .await
             .map_err(|err| ServerError::FailedRead(err.to_string()))?;
     
@@ -237,11 +268,9 @@ impl ChatRepository for MongolDB
     }
 }
 
-fn internal_private_chat_pipeline(filter: Document) -> Vec<Document>
+fn internal_private_chat_pipeline() -> [Document; 4]
 {
-    vec!
     [
-        filter, 
         doc!
         {
             "$project":
@@ -275,11 +304,9 @@ fn internal_private_chat_pipeline(filter: Document) -> Vec<Document>
     ]
 }
 
-fn internal_group_chat_pipeline(filter: Document) -> Vec<Document>
+fn internal_group_chat_pipeline() -> [Document; 6]
 {
-    vec!
     [
-        filter,
         doc!
         {
             "$project":
@@ -331,11 +358,9 @@ fn internal_group_chat_pipeline(filter: Document) -> Vec<Document>
     ]
 }
 
-fn internal_server_chat_pipeline(filter: Document) -> Vec<Document>
+fn internal_server_chat_pipeline() -> [Document; 6]
 {
-    vec!
     [
-        filter,
         doc!
         {
             "$project":
