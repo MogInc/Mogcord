42c8413dcb9cebdfde3a9372c13b6cc04c26cc83
Guillaume Desktop <robertohelper@outlook.com>
2024-07-19 17:37:49 +0200

    fix: re export certain types

--- src/model.rs
+++ src/model.rs
@@ -1,10 +1,15 @@
-pub mod appstate;
+mod appstate;
+mod pagination;
+mod hashing;
+
+pub use appstate::*;
+pub use pagination::*;
+pub use hashing::*;
+
 pub mod chat;
 pub mod error;
-pub mod hashing;
 pub mod log;
 pub mod message;
-pub mod pagination;
 pub mod refresh_token;
 pub mod relation;
 pub mod user;
