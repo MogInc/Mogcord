2fff385d8f7402b8bd027a199da76a9d197c7585
Guillaume Desktop <robertohelper@outlook.com>
2024-07-20 15:58:33 +0200

    feature: implemented create_server

--- src/db/mongol/repositories/server.rs
+++ src/db/mongol/repositories/server.rs
@@ -1,5 +1,6 @@
 use axum::async_trait;
 
+use crate::db::MongolServer;
 use crate::model::error;
 use crate::model::server::{self, Server};
 use crate::db::mongol::MongolDB;
@@ -9,7 +10,13 @@ impl server::Repository for MongolDB
 {
     async fn create_server(&self, server: Server) -> Result<Server, error::Server>
     {
-        todo!()
+        let db_server = MongolServer::try_from(&server)?;
+
+        match self.servers().insert_one(&db_server).await
+        {
+            Ok(_) => Ok(server),
+            Err(err) => Err(error::Server::FailedInsert(err.to_string())),
+        }
     }
     async fn get_server_by_id(&self, server_id: &str) -> Result<Server, error::Server>
     {
