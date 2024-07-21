0d1bdc1d21fa56b238a304537907ff8e0417f9b5
Guillaume Desktop <robertohelper@outlook.com>
2024-07-21 02:15:05 +0200

    feature: added update_chat in repository trait

--- src/model/chat/repository.rs
+++ src/model/chat/repository.rs
@@ -1,13 +1,14 @@
 use axum::async_trait;
 
-use crate::model::error::Server;
+use crate::model::error;
 use super::Chat;
 
 #[async_trait]
 pub trait Repository: Send + Sync 
 {
-    async fn create_chat(&self, chat: Chat) -> Result<Chat, Server>;
-    async fn get_chat_by_id(&self, chat_id: &str) -> Result<Chat, Server>;
-    async fn get_chat_by_chat_info_id(&self, chat_info_id: &str) -> Result<Chat, Server>;
-    async fn does_chat_exist(&self, chat: &Chat) -> Result<bool, Server>;
+    async fn create_chat(&self, chat: Chat) -> Result<Chat, error::Server>;
+    async fn update_chat(&self, chat: Chat) -> Result<(), error::Server>;
+    async fn get_chat_by_id(&self, chat_id: &str) -> Result<Chat, error::Server>;
+    async fn get_chat_by_chat_info_id(&self, chat_info_id: &str) -> Result<Chat, error::Server>;
+    async fn does_chat_exist(&self, chat: &Chat) -> Result<bool, error::Server>;
 }
