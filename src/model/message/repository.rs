79bff7311108984bb8c8954bd1f106445f39edaf
Guillaume Desktop <robertohelper@outlook.com>
2024-07-22 00:13:32 +0200

    refactor: chat_id to channel_id

--- src/model/message/repository.rs
+++ src/model/message/repository.rs
@@ -10,6 +10,6 @@ pub trait Repository: Send + Sync
     async fn create_message(&self, message: Message) -> Result<Message, error::Server>;
     async fn update_message(&self, message: Message) -> Result<Message, error::Server>;
     async fn get_message(&self, message_id: &str) -> Result<Message, error::Server>;
-    async fn get_valid_messages(&self, chat_id: &str, pagination: Pagination) 
+    async fn get_valid_messages(&self, channel_id: &str, pagination: Pagination) 
         -> Result<Vec<Message>, error::Server>;
 }
