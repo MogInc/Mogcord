c06087b74640bdd07d09581a83d055b20142fa28
Guillaume Desktop <robertohelper@outlook.com>
2024-07-21 23:55:32 +0200

    refactor: changed chat info to channel in chat dto, also removed channels

--- src/dto/chat.rs
+++ src/dto/chat.rs
@@ -2,7 +2,7 @@ use serde::Serialize;
 
 use crate::model::chat::Chat;
 
-use super::{ChatInfoCreateResponse, ChatInfoGetResponse, ObjectToDTO};
+use super::{ChannelCreateResponse, ChannelGetResponse, ObjectToDTO};
 
 #[derive(Serialize)]
 pub struct ChatCreateResponse
@@ -18,9 +18,7 @@ pub struct ChatCreateResponse
     #[serde(skip_serializing_if = "Option::is_none")]
     users: Option<Vec<String>>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    chat_info: Option<ChatInfoCreateResponse>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    chat_infos: Option<Vec<ChatInfoCreateResponse>>,
+    channel: Option<ChannelCreateResponse>,
 }
 
 impl ObjectToDTO<Chat> for ChatCreateResponse
@@ -39,8 +37,7 @@ impl ObjectToDTO<Chat> for ChatCreateResponse
                     owner: None,
                     owners: Some(private.owners.into_iter().map(|user| user.id).collect()),
                     users: None,
-                    chat_info: Some(ChatInfoCreateResponse::obj_to_dto(private.chat_info)),
-                    chat_infos: None,
+                    channel: Some(ChannelCreateResponse::obj_to_dto(private.channel)),
                 }
             },
             Chat::Group(group) => 
@@ -53,8 +50,7 @@ impl ObjectToDTO<Chat> for ChatCreateResponse
                     owner: Some(group.owner.id),
                     owners: None,
                     users: Some(group.users.into_iter().map(|user| user.id).collect()),
-                    chat_info: Some(ChatInfoCreateResponse::obj_to_dto(group.chat_info)),
-                    chat_infos: None,
+                    channel: Some(ChannelCreateResponse::obj_to_dto(group.channel)),
                 }
             },
         }
@@ -75,9 +71,7 @@ pub struct ChatGetResponse
     #[serde(skip_serializing_if = "Option::is_none")]
     users: Option<Vec<String>>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    chat_info: Option<ChatInfoGetResponse>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    chat_infos: Option<Vec<ChatInfoGetResponse>>,
+    channel: Option<ChannelGetResponse>,
 }
 
 impl ObjectToDTO<Chat> for ChatGetResponse
@@ -96,8 +90,7 @@ impl ObjectToDTO<Chat> for ChatGetResponse
                     owner: None,
                     owners: Some(private.owners.into_iter().map(|user| user.id).collect()),
                     users: None,
-                    chat_info: Some(ChatInfoGetResponse::obj_to_dto(private.chat_info)),
-                    chat_infos: None,
+                    channel: Some(ChannelGetResponse::obj_to_dto(private.channel)),
                 }
             },
             Chat::Group(group) => 
@@ -110,8 +103,7 @@ impl ObjectToDTO<Chat> for ChatGetResponse
                     owner: Some(group.owner.id),
                     owners: None,
                     users: Some(group.users.into_iter().map(|user| user.id).collect()),
-                    chat_info: Some(ChatInfoGetResponse::obj_to_dto(group.chat_info)),
-                    chat_infos: None,
+                    channel: Some(ChannelGetResponse::obj_to_dto(group.channel)),
                 }
             },
         }
