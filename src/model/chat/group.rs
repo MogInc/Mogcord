78ea75c4dce0894ee983edf4d234286bddac6a37
Guillaume Desktop <robertohelper@outlook.com>
2024-07-23 01:49:36 +0200

    refactor: group user vec to hashmap

--- src/model/chat/group.rs
+++ src/model/chat/group.rs
@@ -1,3 +1,5 @@
+
+use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 
 use crate::model::{channel, error, user::User};
@@ -9,14 +11,14 @@ pub struct Group
     pub id: String,
     pub name: String,
     pub owner: User,
-    pub users: Vec<User>,
+    pub users: HashMap<String, User>,
     pub channel: Channel,
 }
 
 impl Group
 {
     #[must_use]
-    pub fn convert(id: String, name: String, owner: User, users: Vec<User>, channel: Channel) -> Self
+    pub fn convert(id: String, name: String, owner: User, users: HashMap<String, User>, channel: Channel) -> Self
     {
         Self
         {
@@ -30,10 +32,11 @@ impl Group
 
     pub fn new(name: String, owner: User, users: Vec<User>) -> Result<Self, error::Server> 
     {
-        let users_sanitized: Vec<User> = users
+        let users_sanitized = users
             .into_iter()
             .filter(|user| user.id != owner.id)
-            .collect();
+            .map(|user| Ok((user.id.clone(), user)))
+            .collect::<Result<_, _>>()?;
 
         let channel = Channel::new(None);
 
@@ -53,6 +56,11 @@ impl Group
         self.owner.id == user_id
     }
 
+    pub fn is_user_part_of_server(&self, other_user: &str) -> bool
+    {
+        self.is_owner(other_user) || self.users.contains_key(other_user)
+    }
+
     fn internal_is_meeting_requirements(&self) -> Result<(), error::Server> 
     {
         Ok(())
@@ -68,11 +76,11 @@ impl channel::Parent for Group
 
     fn can_read(&self, user_id: &str, _: Option<&str>) -> Result<bool, error::Server> 
     {
-        Ok(self.owner.id == user_id || self.users.iter().any(|user| user.id == user_id))
+        Ok(self.is_user_part_of_server(user_id))
     }
 
     fn can_write(&self, user_id: &str, _: Option<&str>) -> Result<bool, error::Server> 
     {
-        Ok(self.owner.id == user_id || self.users.iter().any(|user| user.id == user_id))
+        Ok(self.is_user_part_of_server(user_id))
     }
 }
