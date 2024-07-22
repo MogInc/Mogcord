3967b7a20713105815cd158cf3a7addca439353d
Guillaume Desktop <robertohelper@outlook.com>
2024-07-22 19:21:40 +0200

    refactor: changed server rights

--- src/model/server/rights.rs
+++ src/model/server/rights.rs
@@ -3,6 +3,5 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, Hash)]
 pub enum Rights
 {
-    Read(bool),
-    Write(bool)
+    ReadChannels(bool),
 }
