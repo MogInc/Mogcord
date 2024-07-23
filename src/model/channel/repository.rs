2d34323f10b4a8d6cc3f61f8e02c93a5dff74154
Guillaume Desktop <robertohelper@outlook.com>
2024-07-23 15:36:16 +0200

    comment: removed commented out methods

--- src/model/channel/repository.rs
+++ src/model/channel/repository.rs
@@ -8,5 +8,4 @@ use super::Channel;
 pub trait Repository: Send + Sync
 {
     async fn get_channel(&self, channel_id: &str) -> Result<Channel, error::Server>;
-    // async fn get_channel_parent(&self, channel_id: &str) -> Result<ParentWrapper, error::Server>;
 }
