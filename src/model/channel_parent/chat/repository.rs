61c83f5e7d0fc7b8f6f77003af05d66916ba511e
Guillaume Desktop <robertohelper@outlook.com>
2024-07-23 20:17:46 +0200

    refactor: using chat now in chat repo

--- src/model/channel_parent/chat/repository.rs
+++ src/model/channel_parent/chat/repository.rs
@@ -1,13 +1,14 @@
 use axum::async_trait;
 
-use crate::model::{channel_parent::ChannelParent, error};
+use crate::model::error;
+use super::Chat;
 
 
 #[async_trait]
 pub trait Repository: Send + Sync 
 {
-    async fn create_chat(&self, chat: ChannelParent) -> Result<ChannelParent, error::Server>;
-    async fn update_chat(&self, chat: ChannelParent) -> Result<(), error::Server>;
-    async fn get_chat_by_id(&self, chat_id: &str) -> Result<ChannelParent, error::Server>;
-    async fn does_chat_exist(&self, chat: &ChannelParent) -> Result<bool, error::Server>;
+    async fn create_chat(&self, chat: Chat) -> Result<Chat, error::Server>;
+    async fn update_chat(&self, chat: Chat) -> Result<(), error::Server>;
+    async fn get_chat_by_id(&self, chat_id: &str) -> Result<Chat, error::Server>;
+    async fn does_chat_exist(&self, chat: &Chat) -> Result<bool, error::Server>;
 }
