d7ab098c0ee689eb02bc752204e4a1be277bb3d2
Guillaume Desktop <robertohelper@outlook.com>
2024-07-23 14:15:28 +0200

    feature: added mongol server try from

--- src/db/mongol/channel_parent/server.rs
+++ src/db/mongol/channel_parent/server.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use bson::Uuid;
 use serde::{Deserialize, Serialize};
 
-use crate::model::channel_parent::Role;
+use crate::{db::helper, model::{channel_parent::{Role, Server}, error}};
 
 //_id gets an ObjectId signed and will most likely do some voodoo to retrieve a chat
 #[derive(Debug, Serialize, Deserialize)]
@@ -17,4 +17,49 @@ pub struct MongolServer
     channel_ids: Vec<Uuid>,
     //key is user id
     roles: HashMap<Uuid, Vec<Role>>,
+}
+
+impl TryFrom<&Server> for MongolServer
+{
+    type Error = error::Server;
+
+    fn try_from(value: &Server) -> Result<Self, Self::Error> 
+    {
+        let db_id = helper::convert_domain_id_to_mongol(&value.id)?;
+
+        let owner_id = helper::convert_domain_id_to_mongol(&value.owner.id)?;
+
+        let user_ids = value
+            .users
+            .keys()
+            .map(|key| helper::convert_domain_id_to_mongol(key))
+            .collect::<Result<_, _>>()?;
+
+        let channel_ids = value
+            .channels
+            .keys()
+            .map(|key| helper::convert_domain_id_to_mongol(key))
+            .collect::<Result<_, _>>()?;
+
+        let roles = value
+            .roles
+            .iter()
+            .map(|(key, val)| {
+                let uuid = helper::convert_domain_id_to_mongol(&key.id)?;
+                Ok((uuid, val.get_all()))
+            })
+            .collect::<Result<_, _>>()?;
+
+        Ok(
+            Self 
+            { 
+                _id: db_id,
+                name: value.name.to_string(),
+                owner_id,
+                user_ids,
+                channel_ids,
+                roles,
+            }
+        )
+    }
 }
