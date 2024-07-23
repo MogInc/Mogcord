55f41334acd1eaf07047d2b471be8974982cb153
Guillaume Desktop <robertohelper@outlook.com>
2024-07-23 20:17:20 +0200

    feature: split channel parent chat part into its own enum

--- src/model/channel_parent/chat.rs
+++ src/model/channel_parent/chat.rs
@@ -1,3 +1,84 @@
 mod repository;
 
 pub use repository::*;
+
+use serde::{Deserialize, Serialize};
+use strum_macros::Display;
+
+use crate::model::{channel::{Channel, Parent}, error, user::User};
+use super::{Group, Private};
+
+#[derive(Clone, Display, Debug, Serialize, Deserialize)]
+pub enum Chat
+{
+    Private(Private),
+    Group(Group),
+}
+
+impl Chat
+{
+    pub fn get_channel(&self, channel_id_option: Option<&str>) -> Result<&Channel, error::Server>
+    {
+        match self
+        {
+            Chat::Private(private) => private.get_channel(channel_id_option),
+            Chat::Group(group) => group.get_channel(channel_id_option),
+        }
+    }
+
+    #[must_use]
+    pub fn private_owner_size() -> usize
+    {
+        Private::owner_size()
+    }
+
+    #[must_use]
+    pub fn is_private(&self) -> bool
+    {
+        matches!(self, Chat::Private(_))
+    }
+
+    #[must_use]
+    pub fn is_group(&self) -> bool
+    {
+        matches!(self, Chat::Group(_))
+    }
+
+    pub fn add_user(&mut self, user: User) -> Result<(), error::Server>
+    {
+        match self
+        {
+            Chat::Private(_) => Err(error::Server::ChatNotAllowedToGainUsers),
+            Chat::Group(group) => group.add_user(user),
+        }
+    }
+
+    pub fn add_users(&mut self, users: Vec<User>) -> Result<(), error::Server>
+    {
+        match self
+        {
+            Chat::Private(_) => Err(error::Server::ChatNotAllowedToGainUsers),
+            Chat::Group(group) => group.add_users(users),
+        }
+    }
+
+    #[must_use]
+    pub fn is_owner(&self, user_id: &str) -> bool
+    {
+        match self
+        {
+            Chat::Private(private) => private.is_owner(user_id),
+            Chat::Group(group) => group.is_owner(user_id),
+        }
+    }
+
+    #[must_use]
+    pub fn is_user_part_of_chat(&self, other_user_id: &str) -> bool
+    {
+        match self
+        {
+            Chat::Private(private) => private.is_owner(other_user_id),
+            Chat::Group(group) => group.is_user_part_of_server(other_user_id),
+        }
+    }
+}
