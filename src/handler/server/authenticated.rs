6bc8b5d8381f9b6f76fc8228465fceb8f7224ccb
Guillaume Desktop <robertohelper@outlook.com>
2024-07-25 01:43:10 +0200

    fix: removed using

--- src/handler/server/authenticated.rs
+++ src/handler/server/authenticated.rs
@@ -1,9 +1,7 @@
 mod create_server;
 mod get_server;
-mod get_server_channel;
 mod join_server;
 
 pub use create_server::*;
 pub use get_server::*;
-pub use get_server_channel::*;
 pub use join_server::*;
