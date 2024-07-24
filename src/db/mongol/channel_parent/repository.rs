1c709894337542e7bc9c626a481ccea022122a4b
Guillaume Desktop <robertohelper@outlook.com>
2024-07-24 02:16:51 +0200

    feature: impl get_chat_by_id in chat repo

--- src/db/mongol/channel_parent/repository.rs
+++ src/db/mongol/channel_parent/repository.rs
@@ -64,7 +64,89 @@ impl channel_parent::chat::Repository for MongolDB
 
     async fn get_chat_by_id(&self, chat_id: &str) -> Result<Chat, error::Server>
     {
-        todo!()
+        let chat_id_local = helper::convert_domain_id_to_mongol(chat_id)?;
+
+        //TODO: refactor this at some point 
+        //currently doing 2 db calls
+        //see if it can be done in 1
+        let filter = doc!
+        {
+            "$or": 
+            [
+                doc!{ "Private._id": chat_id_local },
+                doc!{ "Group._id": chat_id_local },
+            ]
+        };
+
+        let mongol_chat = self
+            .chats()
+            .find_one(filter)
+            .await
+            .map_err(|err| error::Server::FailedRead(err.to_string()))?
+            .ok_or(error::Server::ChatNotFound)?;
+
+        let pipeline = match &mongol_chat
+        {
+            MongolChat::Private { .. } => 
+            {
+                let mut pipeline = vec!
+                [
+                    doc! 
+                    {
+                        "$match":
+                        {
+                            "Private._id": chat_id_local
+                        }
+                    },
+                ];
+
+                pipeline.extend(internal_private_chat_pipeline());
+
+                pipeline
+            },
+            MongolChat::Group { .. } => 
+            {
+                let mut pipeline = vec!
+                [
+                    doc! 
+                    {
+                        "$match":
+                        {
+                            "Group._id": chat_id_local
+                        }
+                    },
+                ];
+
+                pipeline.extend(internal_group_chat_pipeline());
+
+                pipeline
+            },
+        };
+            
+
+        let mut cursor = self
+            .chats()
+            .aggregate(pipeline)
+            .await
+            .map_err(|err| error::Server::FailedRead(err.to_string()))?;
+    
+        let document_option = cursor
+            .next()
+            .await
+            .transpose()
+            .map_err(|err| error::Server::UnexpectedError(err.to_string()))?;
+
+        match document_option
+        {
+            Some(document) => 
+            {
+                let chat = from_document(document)
+                    .map_err(|err| error::Server::UnexpectedError(err.to_string()))?;
+
+                return Ok(chat);
+            },
+            None => Err(error::Server::ChatNotFound), 
+        }
     }
 
     async fn does_chat_exist(&self, chat: &Chat) -> Result<bool, error::Server>
@@ -121,4 +203,81 @@ impl channel_parent::server::Repository for MongolDB
     {
         todo!()
     }
+}
+
+
+fn internal_private_chat_pipeline() -> [Document; 3]
+{
+    [
+        doc! 
+        {
+            "$lookup": 
+            {
+                "from": "users",
+                "localField": "Private.owner_ids",
+                "foreignField": "_id",
+                "as": "Private.owners"
+            }
+        },
+        doc!
+        {
+            "$addFields":
+            {
+                "Private.id": map_mongo_key_to_string!("$_id", "uuid"),
+                "Private.chat_info.id": map_mongo_key_to_string!("$Private.chat_info._id", "uuid"),
+                "Private.owners": map_mongo_collection_keys_to_string!("$Private.owners", "_id", "id", "uuid"),
+            }
+        },
+        doc!
+        {
+            "$unset": ["_id", "Private.owner_ids", "Private.owners._id", "Private.chat_info._id"]
+        }
+    ]
+}
+
+fn internal_group_chat_pipeline() -> [Document; 5]
+{
+    [
+        doc! 
+        {
+            "$lookup": 
+            {
+                "from": "users",
+                "localField": "Group.owner_id",
+                "foreignField": "_id",
+                "as": "Group.owner"
+            }
+        },
+        doc!
+        {
+            "$unwind":
+            {
+                "path": "$Group.owner"
+            }
+        },
+        doc!
+        {
+            "$lookup":
+            {
+                "from": "users",
+                "localField": "Group.user_ids",
+                "foreignField": "_id",
+                "as": "Group.users"
+            }
+        },
+        doc!
+        {
+            "$addFields":
+            {
+                "Group.id": map_mongo_key_to_string!("$_id", "uuid"),
+                "Group.owner.id": map_mongo_key_to_string!("$Group.owner._id", "uuid"),
+                "Group.chat_info.id": map_mongo_key_to_string!("$Group.chat_info._id", "uuid"),
+                "Group.users": map_mongo_collection_keys_to_string!("$Group.users", "_id", "id", "uuid"),
+            }
+        },
+        doc!
+        {
+            "$unset": ["_id", "Group.owner_id", "Group.user_ids", "Group.owner._id",  "Group.chat_info._id"]
+        }
+    ]
 }
