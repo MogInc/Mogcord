437dfc66327070346bc8b198684e41c56b921782
Guillaume Desktop <robertohelper@outlook.com>
2024-07-25 00:53:48 +0200

    feature: create_server server endpoint for #24

--- src/handler/server/authenticated/create_server.rs
+++ src/handler/server/authenticated/create_server.rs
@@ -2,6 +2,8 @@ use std::sync::Arc;
 use axum::{extract::State, response::IntoResponse, Json};
 use serde::Deserialize;
 
+use crate::dto::{ObjectToDTO, ServerCreateResponse};
+use crate::model::channel_parent::server;
 use crate::model::AppState;
 use crate::middleware::auth::Ctx;
 
@@ -17,5 +19,20 @@ pub async fn create_server(
     Json(payload): Json<CreateServerRequest>
 ) -> impl IntoResponse
 {
-    todo!()
+    let repo_server = &state.servers;
+    let repo_user = &state.users;
+
+    let ctx_user_id = &ctx.user_id();
+
+    let owner = repo_user
+        .get_user_by_id(ctx_user_id)
+        .await?;
+
+    let server = server::Server::new(payload.name, owner)?;
+
+    match repo_server.create_server(server).await 
+    {
+        Ok(server) => Ok(Json(ServerCreateResponse::obj_to_dto(server))),
+        Err(err) => Err(err),
+    }
 }
