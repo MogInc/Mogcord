e0379acd389961e489507c71c51f3186f1677984
Guillaume Desktop <robertohelper@outlook.com>
2024-07-25 12:53:32 +0200

    feature: chat now impls channel::parent trait, just bubbles to individual trait calls

--- src/model/channel_parent/chat.rs
+++ src/model/channel_parent/chat.rs
@@ -9,7 +9,7 @@ pub use private::*;
 use serde::{Deserialize, Serialize};
 use strum_macros::Display;
 
-use crate::model::{channel::{Channel, Parent}, error, user::User};
+use crate::model::{channel::{self, Channel}, error, user::User};
 
 #[derive(Clone, Display, Debug, Serialize, Deserialize)]
 pub enum Chat
@@ -20,15 +20,6 @@ pub enum Chat
 
 impl Chat
 {
-    pub fn get_channel(&self, channel_id_option: Option<&str>) -> Result<&Channel, error::Server>
-    {
-        match self
-        {
-            Chat::Private(private) => private.get_channel(channel_id_option),
-            Chat::Group(group) => group.get_channel(channel_id_option),
-        }
-    }
-
     #[must_use]
     pub fn private_owner_size() -> usize
     {
@@ -82,6 +73,36 @@ impl Chat
         {
             Chat::Private(private) => private.is_owner(other_user_id),
             Chat::Group(group) => group.is_user_part_of_server(other_user_id),
+        }
+    }
+}
+
+impl channel::Parent for Chat
+{
+    fn get_channel(&self, channel_id_option: Option<&str>) -> Result<&Channel, error::Server>
+    {
+        match self
+        {
+            Chat::Private(private) => private.get_channel(channel_id_option),
+            Chat::Group(group) => group.get_channel(channel_id_option),
+        }
+    }
+
+    fn can_read(&self, user_id: &str, channel_id_option: Option<&str>) -> Result<bool, error::Server> 
+    {
+        match self
+        {
+            Chat::Private(val) => val.can_read(user_id, channel_id_option),
+            Chat::Group(val) => val.can_read(user_id, channel_id_option),
+        }
+    }
+
+    fn can_write(&self, user_id: &str, channel_id_option: Option<&str>) -> Result<bool, error::Server> 
+    {
+        match self
+        {
+            Chat::Private(val) => val.can_write(user_id, channel_id_option),
+            Chat::Group(val) => val.can_write(user_id, channel_id_option),
         }
     }
 }
