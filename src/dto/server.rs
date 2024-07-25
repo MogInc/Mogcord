860cf9fc0cf465761ec362e7fa51e0432a4806ba
Guillaume Desktop <robertohelper@outlook.com>
2024-07-25 21:04:45 +0200

    feature: server get response options gone and added roles and user roles

--- src/dto/server.rs
+++ src/dto/server.rs
@@ -1,6 +1,8 @@
+use std::collections::HashMap;
+
 use serde::Serialize;
 
-use crate::model::channel_parent::Server;
+use crate::model::channel_parent::{Role, Server};
 
 use super::{vec_to_dto, ChannelCreateResponse, ChannelGetResponse, ObjectToDTO};
 
@@ -40,14 +42,14 @@ pub struct ServerGetResponse
 {
     id: String,
     r#type: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    name: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    owner: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    users: Option<Vec<String>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    channels: Option<Vec<ChannelGetResponse>>,
+    name: String,
+    owner: String,
+    users: Vec<String>,
+    channels: Vec<ChannelGetResponse>,
+    roles: Vec<Role>,
+    //key user id
+    //value role names
+    user_roles: HashMap<String, Vec<String>>,
 }
 
 impl ObjectToDTO<Server> for ServerGetResponse
@@ -58,10 +60,12 @@ impl ObjectToDTO<Server> for ServerGetResponse
         {
             id: model_input.id,
             r#type: String::from("Server"),
-            name: Some(model_input.name),
-            owner: Some(model_input.owner.id),
-            users: Some(model_input.users.into_keys().collect()),
-            channels: Some(vec_to_dto(model_input.channels.into_values().collect())),
+            name: model_input.name,
+            owner: model_input.owner.id,
+            users: model_input.users.into_keys().collect(),
+            channels: vec_to_dto(model_input.channels.into_values().collect()),
+            roles: model_input.roles.into_values().collect(),
+            user_roles: model_input.user_roles,
         }
     }
 }
