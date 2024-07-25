9bbaa38768356fc8dfe4229630bfdf7adf44821b
Guillaume Desktop <robertohelper@outlook.com>
2024-07-25 12:54:38 +0200

    feature: ChannelParent now impls channel::parent trait, just bubbles to individual trait calls

--- src/model/channel_parent.rs
+++ src/model/channel_parent.rs
@@ -14,7 +14,7 @@ use serde::{Deserialize, Serialize};
 use strum_macros::Display;
 
 use crate::model::user::User;
-use super::{channel::{Channel, Parent}, error};
+use super::{channel::{self, Channel}, error};
 
 
 #[derive(Clone, Display, Debug, Serialize, Deserialize)]
@@ -27,15 +27,6 @@ pub enum ChannelParent
 
 impl ChannelParent
 {
-    pub fn get_channel(&self, channel_id_option: Option<&str>) -> Result<&Channel, error::Server>
-    {
-        match self
-        {
-            ChannelParent::Chat(chat) => chat.get_channel(channel_id_option),
-            ChannelParent::Server(server) => server.get_channel(channel_id_option),
-        }
-    }
-
     #[must_use]
     pub fn is_chat(&self) -> bool
     {
@@ -83,6 +74,36 @@ impl ChannelParent
         {
             ChannelParent::Chat(value) => value.is_user_part_of_chat(other_user_id),
             ChannelParent::Server(value) => value.is_user_part_of_server(other_user_id),
+        }
+    }
+}
+
+impl channel::Parent for ChannelParent
+{
+    fn get_channel(&self, channel_id_option: Option<&str>) -> Result<&Channel, error::Server> 
+    {
+        match self
+        {
+            ChannelParent::Chat(val) => val.get_channel(channel_id_option),
+            ChannelParent::Server(val) => val.get_channel(channel_id_option),
+        }
+    }
+
+    fn can_read(&self, user_id: &str, channel_id_option: Option<&str>) -> Result<bool, error::Server> 
+    {
+        match self
+        {
+            ChannelParent::Chat(val) => val.can_read(user_id, channel_id_option),
+            ChannelParent::Server(val) => val.can_read(user_id, channel_id_option),
+        }
+    }
+
+    fn can_write(&self, user_id: &str, channel_id_option: Option<&str>) -> Result<bool, error::Server> 
+    {
+        match self
+        {
+            ChannelParent::Chat(val) => val.can_write(user_id, channel_id_option),
+            ChannelParent::Server(val) => val.can_write(user_id, channel_id_option),
         }
     }
 }
