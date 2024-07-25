a815b38232871d53a41c3907532d0f373b4f848e
Guillaume Desktop <robertohelper@outlook.com>
2024-07-25 12:48:46 +0200

    feature: added get_channel_parent to repo trait

--- src/model/channel_parent/repository.rs
+++ src/model/channel_parent/repository.rs
@@ -1,9 +1,11 @@
 use axum::async_trait;
 
-use super::{chat, server};
+use crate::model::error;
+
+use super::{chat, server, ChannelParent};
 
 #[async_trait]
 pub trait Repository: Send + Sync + server::Repository + chat::Repository
 {
-
+    async fn get_channel_parent(&self, channel_id: &str) -> Result<ChannelParent, error::Server>;
 }
