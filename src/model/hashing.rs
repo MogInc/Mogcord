66802ff53dfc6c32ade767faea512b6c7ea5f9c9
Guillaume Desktop <robertohelper@outlook.com>
2024-07-26 16:15:17 +0200

    refactor: better error handling in hashing for #19

--- src/model/hashing.rs
+++ src/model/hashing.rs
@@ -13,7 +13,7 @@ pub struct Hashing;
 
 impl Hashing
 {
-    pub async fn hash_text(clear_text: &str) -> Result<String, error::Server>
+    pub async fn hash_text<'input, 'stack>(clear_text: &'input str) -> Result<String, error::Server<'stack>>
     {
         let clear_text = clear_text.to_string();
 
@@ -25,15 +25,26 @@ impl Hashing
 
             //no need to return the salt, its stored inside the hash
             return argon2.hash_password(clear_text.as_bytes(), &salt)
-                .map_err(|_| error::Server::HashingPasswordFailed)
+                .map_err(|_| error::Server::new(
+                    error::Kind::Create,
+                    error::OnType::Hashing,
+                    "Model.Hashing::hash_text",
+                    line!()
+                ))
                 .map(|hash| hash.to_string());
             
-        }).await.map_err(|_| error::Server::HashingPasswordFailedBlocking)??;
+        }).await.map_err(|err| error::Server::new(
+            error::Kind::Unexpected,
+            error::OnType::SpawnBlocking,
+            "Model.Hashing::hash_text",
+            line!())
+            .add_extra_info(err.to_string())
+        )??;
 
         Ok(text_hashed)
     }
 
-    pub async fn verify_hash(clear_text: &str, hash: &str) -> Result<(), error::Server>
+    pub async fn verify_hash<'input, 'stack>(clear_text: &'input str, hash: &'input str) -> Result<(), error::Server<'stack>>
     {
         let clear_text = clear_text.to_string();
         let hash = hash.to_string();
@@ -41,13 +52,28 @@ impl Hashing
         task::spawn_blocking(move || 
         {
             let parsed_hash = PasswordHash::new(&hash)
-                .map_err(|_| error::Server::VerifyingPasswordFailed)?;
+                .map_err(|_| error::Server::new(
+                    error::Kind::Verifying,
+                    error::OnType::Hashing,
+                    "Model.Hashing::verify_hash",
+                    line!(),
+                ))?;
             
             let argon2 = Self::internal_give_argon_settings();
 
             argon2.verify_password(clear_text.as_bytes(), &parsed_hash)
-                  .map_err(|_| error::Server::VerifyingPasswordFailed)
-        }).await.map_err(|_| error::Server::VerifyingPasswordFailedBlocking)??;
+                  .map_err(|_| error::Server::new(
+                    error::Kind::Verifying,
+                    error::OnType::Hashing,
+                    "Model.Hashing::verify_hash",
+                    line!(),
+                    ))
+        }).await.map_err(|_| error::Server::new(
+            error::Kind::Unexpected,
+            error::OnType::SpawnBlocking,
+            "Model.Hashing::verify_hash",
+            line!(),
+        ))??;
 
         Ok(())
     }
