cb5297e478662c92bd3de918579d111e8ee95408
Guillaume Desktop <robertohelper@outlook.com>
2024-07-29 00:52:10 +0200

    docu: added info for filewriter

--- src/file_writer.rs
+++ src/file_writer.rs
@@ -8,11 +8,47 @@ pub struct FileWriter<'a>
 impl<'a> FileWriter<'a>
 {
     #[must_use]
+    /// Creates a new instance of `FileWriter` and ensures the specified folder path exists.
+    /// 
+    /// # Parameters
+    /// 
+    /// - `folder_path`: A string slice that holds the path to the folder.
+    ///  
+    /// NOTE: This path is ensured to exist by creating the directory and any necessary parent directories if they do not already exist.
+    /// # Returns
+    /// 
+    /// - A new instance of `FileWriter`.
+    /// 
+    /// # Panics
+    /// 
+    /// This function will panic if it fails to create the directory or any of its parent directories.
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// # use mogcord::file_writer::FileWriter;
+    /// 
+    /// let path = "/path/to/folder";
+    /// let fw = FileWriter::new(path);
+    /// 
+    /// assert_eq!(path, fw.get_path());
+    /// ```
     pub fn new(folder_path: &'a str) -> Self
     {
+        std::fs::create_dir_all(folder_path)
+            .expect("failed to create");
         Self
         {
             folder_path
         }
+    }
+}
+
+impl<'a> FileWriter<'a>
+{
+    #[must_use]
+    pub fn get_path(&self) -> &str
+    {
+        self.folder_path
     }
 }
