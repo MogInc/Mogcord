ff72c6b30da740992667f270587253798b73c807
Guillaume Desktop <robertohelper@outlook.com>
2024-07-28 20:10:50 +0200

    feature: added bubble macro to channel repo

--- src/db/mongol/channel/repository.rs
+++ src/db/mongol/channel/repository.rs
@@ -4,7 +4,7 @@ use bson::doc;
 use crate::model::channel::Channel;
 use crate::model::{channel, error};
 use crate::db::mongol::MongolDB;
-use crate::server_error;
+use crate::{bubble, server_error};
 use super::helper;
 
 #[async_trait]
@@ -12,7 +12,7 @@ impl channel::Repository for MongolDB
 {
     async fn get_channel<'input, 'err>(&'input self, channel_id: &'input str) -> Result<Channel, error::Server<'err>>
     {
-        let channel_id_local = helper::convert_domain_id_to_mongol(channel_id)?;
+        let channel_id_local = bubble!(helper::convert_domain_id_to_mongol(channel_id))?;
 
         let filter = doc! { "_id": channel_id_local };
 
