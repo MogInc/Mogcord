5efdb923d11b6859e820f5b9a393e1a9f056b532
Guillaume Desktop <robertohelper@outlook.com>
2024-07-28 20:19:50 +0200

    feature: added bubble to group

--- src/db/mongol/channel_parent/chat/group.rs
+++ src/db/mongol/channel_parent/chat/group.rs
@@ -1,7 +1,7 @@
 use bson::Uuid;
 use serde::{Deserialize, Serialize};
 
-use crate::{db::helper, model::{channel_parent::chat::Group, error}};
+use crate::{bubble, db::helper, model::{channel_parent::chat::Group, error}};
 
 //_id gets an ObjectId signed and will most likely do some voodoo to retrieve a chat
 #[derive(Debug, Serialize, Deserialize)]
@@ -22,15 +22,15 @@ impl TryFrom<&Group> for MongolGroup
 
     fn try_from(value: &Group) -> Result<Self, Self::Error> 
     {
-        let db_id = helper::convert_domain_id_to_mongol(&value.id)?;
+        let db_id = bubble!(helper::convert_domain_id_to_mongol(&value.id))?;
 
-        let channel_id = helper::convert_domain_id_to_mongol(&value.channel.id)?;
+        let channel_id = bubble!(helper::convert_domain_id_to_mongol(&value.channel.id))?;
 
-        let owner_id = helper::convert_domain_id_to_mongol(&value.owner.id)?;
+        let owner_id = bubble!(helper::convert_domain_id_to_mongol(&value.owner.id))?;
 
         let user_ids = value.users
             .keys()
-            .map(|key| helper::convert_domain_id_to_mongol(key))
+            .map(|key| bubble!(helper::convert_domain_id_to_mongol(key)))
             .collect::<Result<_, _>>()?;
 
         Ok(
