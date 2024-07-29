40d9948b18cd70779784f1d6983951886599ae60
Guillaume Desktop <robertohelper@outlook.com>
2024-07-30 01:47:58 +0200

    refactor: cleaned up main by moving new into appstate

--- src/model/appstate.rs
+++ src/model/appstate.rs
@@ -1,5 +1,7 @@
 use std::sync::Arc;
 
+use crate::db::MongolDB;
+
 use super::{channel, channel_parent, message, refresh_token, relation, user};
 
 
@@ -13,4 +15,39 @@ pub struct AppState
     pub messages: Arc<dyn message::Repository>,
     pub refresh_tokens: Arc<dyn refresh_token::Repository>,
     pub relations: Arc<dyn relation::Repository>,
+}
+
+impl AppState
+{
+    pub async fn new(db_con: &str) -> Arc<Self>
+    {
+        let db = Arc::new(
+        MongolDB::init(db_con)
+                .await
+                .expect("Couldnt connect to db")
+        );
+    
+        let chats =  Arc::clone(&db) as Arc<dyn channel_parent::Repository>;
+        let servers =  Arc::clone(&db) as Arc<dyn channel_parent::Repository>;
+        let channel_parents =  Arc::clone(&db) as Arc<dyn channel_parent::Repository>;
+        let channels =  Arc::clone(&db) as Arc<dyn channel::Repository>;
+        let users = Arc::clone(&db) as Arc<dyn user::Repository>;
+        let messages = Arc::clone(&db) as Arc<dyn message::Repository>;
+        let refresh_tokens = Arc::clone(&db) as Arc<dyn refresh_token::Repository>;
+        let relations = Arc::clone(&db) as Arc<dyn relation::Repository>;
+
+        Arc::new(
+            Self 
+            {
+                chats,
+                servers,
+                channel_parents,
+                channels,
+                users,
+                messages,
+                refresh_tokens,
+                relations,
+            }
+        )
+    }
 }
