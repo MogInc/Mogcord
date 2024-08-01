51d43a68b491641f087ed3fb8e7ec26e41c9e19e
Guillaume Desktop <robertohelper@outlook.com>
2024-08-02 00:44:37 +0200

    refactor: mtx resolver now takes in a state

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,6 +1,6 @@
-pub mod api;
-pub mod web;
-mod logic;
+mod api;
+mod web;
+pub mod logic;
 
 use std::sync::Arc;
 use axum::{http::StatusCode, middleware, response::IntoResponse, routing::Router};
@@ -8,13 +8,13 @@ use tower_cookies::CookieManagerLayer;
 
 use crate::{middleware::{auth::mw_ctx_resolver, logging::main_response_mapper}, model::AppState};
 
-pub fn new(state: &Arc<AppState>) -> Router
+pub fn new(state: Arc<AppState>) -> Router
 {
     Router::new()
         .nest("/", web::routes(state.clone()))
         .nest("/api", api::routes(state.clone()))
         .layer(middleware::map_response_with_state(state.logs.clone(), main_response_mapper))
-        .layer(middleware::from_fn(mw_ctx_resolver))
+        .layer(middleware::from_fn_with_state(state, mw_ctx_resolver))
         .layer(CookieManagerLayer::new())
         .fallback(page_not_found)
 }
