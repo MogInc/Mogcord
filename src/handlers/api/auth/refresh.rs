93f5d6063570a029dd27981dfdccd2462897b15e
Guillaume Desktop <robertohelper@outlook.com>
2024-08-02 01:47:27 +0200

    feature: refresh api endpoint now short circuits if ctx is valid

--- src/handlers/api/auth/refresh.rs
+++ src/handlers/api/auth/refresh.rs
@@ -2,13 +2,20 @@ use std::sync::Arc;
 use axum::{extract::State, response::IntoResponse};
 use tower_cookies::Cookies;
 
+use crate::middleware::auth::Ctx;
 use crate::model::AppState;
 use crate::handlers;
 
 pub async fn refresh_token(
     State(state): State<Arc<AppState>>,
+    ctx_option: Option<Ctx>,
     jar: Cookies
 ) -> impl IntoResponse
 {
+    if ctx_option.is_some()
+    {
+        return Ok(());
+    }
+
     handlers::logic::auth::refresh_token(&state, &jar).await
 }
