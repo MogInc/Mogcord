7fde5d92bdfcc457c7432b96dc5fa739cc0fbe84
Guillaume Desktop <robertohelper@outlook.com>
2024-08-03 21:48:57 +0200

    refactor: removed method that checks if logged in and uses option ctx

--- src/handlers/web/auth/login.rs
+++ src/handlers/web/auth/login.rs
@@ -6,7 +6,7 @@ use axum_htmx::HxRedirect;
 use serde::Deserialize;
 use tower_cookies::Cookies;
 
-use crate::{handlers::{logic, web::HtmxError}, model::AppState};
+use crate::{handlers::{logic, web::HtmxError}, middleware::auth::Ctx, model::AppState};
 
 #[derive(Template)]
 #[template(path = "login.html")]
@@ -14,9 +14,12 @@ pub struct Login
 {
 
 }
-pub async fn get_login(jar: Cookies) -> Result<Login, HtmxError>
+pub async fn get_login(ctx_option: Option<Ctx>) -> Result<Login, HtmxError>
 {
-    super::is_logged_in(&jar)?;
+    if ctx_option.is_some()
+    {
+        return Err(HtmxError::new(crate::model::error::Client::USER_ALREADY_LOGGED_IN));
+    }
 
     Ok(Login{})
 }
@@ -30,10 +33,14 @@ pub struct LoginRequest
 pub async fn post_login(
     State(state): State<Arc<AppState>>,
     jar: Cookies,
+    ctx_option: Option<Ctx>,
     Form(form): Form<LoginRequest>
 ) -> Result<impl IntoResponse, HtmxError>
 {
-    super::is_logged_in(&jar)?;
+    if ctx_option.is_some()
+    {
+        return Err(HtmxError::new(crate::model::error::Client::USER_ALREADY_LOGGED_IN));
+    }
 
     let login_result = logic::auth::login(state, jar, &form.mail, &form.password).await;
 
