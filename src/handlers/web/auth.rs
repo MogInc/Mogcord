96e97b539d23523d8eed19843410e0febb37798f
Guillaume Desktop <robertohelper@outlook.com>
2024-08-03 15:23:40 +0200

    refactor: moved is_logged_in into parent

--- src/handlers/web/auth.rs
+++ src/handlers/web/auth.rs
@@ -1,3 +1,18 @@
 mod login;
+mod logout;
 
 pub use login::*;
+pub use logout::*;
+
+
+fn is_logged_in(jar: &tower_cookies::Cookies) -> Result<(), crate::model::error::Client>
+{
+    let ctx_result = crate::middleware::auth::get_ctx(jar);
+
+    if ctx_result.is_ok()
+    {
+        return Err(crate::model::error::Client::USER_ALREADY_LOGGED_IN);
+    }
+
+    Ok(())
+}
