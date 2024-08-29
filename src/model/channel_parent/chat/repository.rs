5d7f97c62d416c18d89ac61f22405dfaafc829f5
Guillaume Desktop <robertohelper@outlook.com>
2024-08-29 16:37:12 +0200

    feature: added get_chats_by_user

--- src/model/channel_parent/chat/repository.rs
+++ src/model/channel_parent/chat/repository.rs
@@ -8,10 +8,17 @@ pub trait Repository: Send + Sync
 {
     async fn create_chat<'input, 'err>(&'input self, chat: Chat) -> error::Result<'err, Chat>;
     async fn update_chat<'input, 'err>(&'input self, chat: Chat) -> error::Result<'err, ()>;
+
     async fn get_chat_by_id<'input, 'err>(
         &'input self,
         chat_id: &'input str,
     ) -> error::Result<'err, Chat>;
+
+    async fn get_chats_by_user<'input, 'err>(
+        &'input self,
+        user_id: &'input str,
+    ) -> error::Result<'err, Vec<Chat>>;
+
     async fn does_chat_exist<'input, 'err>(
         &'input self,
         chat: &'input Chat,
