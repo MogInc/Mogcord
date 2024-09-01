127426acf7b0a149db3173ebcfd4aa4d4ac03465
Guillaume Desktop <robertohelper@outlook.com>
2024-09-01 17:22:11 +0200

    refactor: api get_messages now makes use of logic get_messages

--- src/handlers/api/message/authenticated/get_messages.rs
+++ src/handlers/api/message/authenticated/get_messages.rs
@@ -4,11 +4,10 @@ use axum::Json;
 use std::sync::Arc;
 
 use crate::dto::{vec_to_dto, MessageGetResponse};
+use crate::handlers::logic;
 use crate::middleware::auth::Ctx;
-use crate::model::channel::Parent;
 use crate::model::message::Message;
-use crate::model::{error, AppState, Pagination};
-use crate::server_error;
+use crate::model::{AppState, Pagination};
 
 pub async fn get_messages(
     State(state): State<Arc<AppState>>,
@@ -17,29 +16,8 @@ pub async fn get_messages(
     pagination: Option<Query<Pagination>>,
 ) -> impl IntoResponse
 {
-    let repo_message = &state.messages;
-    let repo_parent = &state.channel_parents;
-
     let pagination = Pagination::new(pagination);
-    let current_user_id = ctx.user_id_ref();
-
-    let chat = repo_parent.get_channel_parent(&channel_id).await?;
-
-    if !chat.is_user_part_of_channel_parent(current_user_id)
-    {
-        return Err(server_error!(error::Kind::NotPartOf, error::OnType::ChannelParent)
-            .add_client(error::Client::SERVER_CTX_NOT_PART_OF_SERVER));
-    }
-
-    if !chat.can_read(current_user_id, Some(&channel_id))?
-    {
-        return Err(server_error!(error::Kind::NotPartOf, error::OnType::ChannelParent)
-            .add_client(error::Client::SERVER_CTX_NOT_PART_OF_SERVER));
-    }
-
-    match repo_message
-        .get_valid_messages(&channel_id, pagination)
-        .await
+    match logic::message::authenticated::get_messages(&state, &channel_id, &ctx, &pagination).await
     {
         Ok(messages) => Ok(Json(vec_to_dto::<Message, MessageGetResponse>(messages))),
         Err(e) => Err(e),
